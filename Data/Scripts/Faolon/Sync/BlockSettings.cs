5f61e8bbdd3814bfaf2a5c096ab8fdc619608fb2
Faolon Briare <38812616+Faolon12@users.noreply.github.com>
2023-12-31 00:17:33 -0800

    Add files via upload

---
+++ Data/Scripts/Faolon/Sync/BlockSettings.cs
@@ -0,0 +1,35 @@
+﻿using ProtoBuf;
+using System.Collections.Generic;
+using VRageMath;
+
+namespace FaolonTether.PowerCables
+{
+    [ProtoContract(UseProtoMembersOnly = true)]
+    public class PowerCableBlockSettings
+    {
+        //[ProtoMember(1)]
+        //public List<PowerCable.Connection> cable_draw;
+
+        [ProtoMember(1)]
+        public long ConnectedBlockId;
+
+        [ProtoMember(2)]
+        public Vector3D ConnectedBlockAttachLocation;
+
+        [ProtoMember(3)]
+        public long ConnectionId;
+
+        // PLACEHOLDER for multi-connection tether blocks.
+        [ProtoMember(4)]
+        public long ConnectedBlockIdA;
+
+        [ProtoMember(5)]
+        public long ConnectedBlockIdB;
+
+        [ProtoMember(6)]
+        public long ConnectedBlockIdC;
+
+        [ProtoMember(7)]
+        public long ConnectedBlockIdD;
+    }
+}
---
+++ Data/Scripts/Faolon/Sync/Mod.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using VRage.Game.Components;
+using VRage.Game.Entity;
+using VRage.Utils;
+
+using FaolonTether.PowerCables.Sync;
+
+namespace FaolonTether.PowerCables
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class PowerCableMod : MySessionComponentBase
+    {
+        public static PowerCableMod Instance;
+
+        public bool ControlsCreated = false;
+        public bool CockpitControlsCreated = false;
+        public bool RemoteControlsCreated = false;
+        public Networking Networking = new Networking(58936);
+        public List<MyEntity> Entities = new List<MyEntity>();
+        public PacketBlockSettings CachedPacketSettings;
+
+        public readonly MyStringId MATERIAL_SQUARE = MyStringId.GetOrCompute("Square");
+        public readonly MyStringId MATERIAL_DOT = MyStringId.GetOrCompute("WhiteDot");
+
+        public override void LoadData()
+        {
+            Instance = this;
+
+            Networking.Register();
+
+            CachedPacketSettings = new PacketBlockSettings();
+        }
+
+        protected override void UnloadData()
+        {
+            Instance = null;
+
+            Networking?.Unregister();
+            Networking = null;
+        }
+    }
+}
